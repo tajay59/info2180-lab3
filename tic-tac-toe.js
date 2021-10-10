76c8e4ce2db75ccd6507a7f20f41526c83b9cbcb
LEGENDARY <tajay59@live.com>
2021-10-10 16:51:15 -0500

    Exercise 2 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,22 @@
- 
+
  window.onload = (event) => {
     console.log('DOM fully loaded and parsed');
+    nextplay = false;                                                   // Used to keep track of whether to play 'X' or 'O' next
+
+    boardplays = new Array(9);                                          // Array used to store each play
     const board = document.querySelectorAll('#board > div');  
-    board.forEach(item => item.classList.add("square"));
+    board.forEach((item,index ) => {item.classList.add("square");});    // Add .square class to each child div of #board
+
+    board.forEach((item,index ) => { item.addEventListener("click",()=> {
+        let play = (nextplay == false)?  "X" : "O"                      // Toggle between 'X' and 'O' for each play
+        item.classList.add(play);                                       // Add appropriate styling to each 'X' or 'O' play based on class in css file
+        boardplays[index] = play;                                       // Update array with current play 
+        nextplay = !nextplay;                                           // Toggle next play
+        item.innerHTML = play;                                          // Print cuurent play to clicked square
+    
+    })});
 };
- 
- 
+
+
  
  
