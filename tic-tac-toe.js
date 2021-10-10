bfd0d96145c8946bac428ae7f8816aa974fde505
LEGENDARY <tajay59@live.com>
2021-10-10 17:04:48 -0500

    Exercise 3 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,14 +7,24 @@
     const board = document.querySelectorAll('#board > div');  
     board.forEach((item,index ) => {item.classList.add("square");});    // Add .square class to each child div of #board
 
-    board.forEach((item,index ) => { item.addEventListener("click",()=> {
-        let play = (nextplay == false)?  "X" : "O"                      // Toggle between 'X' and 'O' for each play
-        item.classList.add(play);                                       // Add appropriate styling to each 'X' or 'O' play based on class in css file
-        boardplays[index] = play;                                       // Update array with current play 
-        nextplay = !nextplay;                                           // Toggle next play
-        item.innerHTML = play;                                          // Print cuurent play to clicked square
-    
-    })});
+    board.forEach((item,index ) => { 
+        item.addEventListener("click",()=> {
+            let play = (nextplay == false)?  "X" : "O"                   // Toggle between 'X' and 'O' for each play
+            item.classList.add(play);                                    // Add appropriate styling to each 'X' or 'O' play based on class in css file
+            boardplays[index] = play;                                    // Update array with current play 
+            nextplay = !nextplay;                                        // Toggle next play
+            item.innerHTML = play;                                       // Print cuurent play to clicked square    
+            });
+        
+        item.addEventListener("mouseover",()=> { 
+            item.classList.add("hover");                                 // Add appropriate styling to each square on mouseover   
+            } );     
+ 
+        item.addEventListener("mouseout",()=> { 
+            item.classList.remove("hover");                               // Add appropriate styling to each square on mouseout   
+            } ); 
+        
+        });
 };
 
 
