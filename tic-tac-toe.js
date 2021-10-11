4e70b89769178b7dc0947b6410dd6d6aa6e792ab
LEGENDARY <tajay59@live.com>
2021-10-10 20:22:55 -0500

    Exercise 6 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,17 @@
 
- window.onload = (event) => {
-    console.log('DOM fully loaded and parsed');
-    nextplay        = false;                                               // Used to keep track of whether to play 'X' or 'O' next
-    const status    = document.querySelector("#status");                   // Status selector for updating winner of the game
-    const button    = document.querySelector(".btn");                      // Button selector for restarting the game
-    boardplays      = new Array(9);                                        // Array used to store each play
+ window.onload = (event) => { 
+    let playOn      = true;                                              // Game continues as long as this variable in 'true'
+    nextplay        = false;                                             // Used to keep track of whether to play 'X' or 'O' next
+    const status    = document.querySelector("#status");                 // Status selector for updating winner of the game
+    const button    = document.querySelector(".btn");                    // Button selector for restarting the game
+    boardplays      = new Array(9);                                      // Array used to store each play / history
     const board     = document.querySelectorAll('#board > div');  
 
-    board.forEach((item,index ) => {item.classList.add("square");});       // Add .square class to each child div of #board
+    board.forEach((item,index ) => {item.classList.add("square");});     // Add .square class to each child div of #board
 
     board.forEach((item,index ) => { 
         item.addEventListener("click",()=> {
+            if((boardplays[index] === undefined) && playOn ){            // Process new play only if selected square is empty
             let play = (nextplay == false)?  "X" : "O"                   // Toggle between 'X' and 'O' for each play
             item.classList.add(play);                                    // Add appropriate styling to each 'X' or 'O' play based on class in css file
             boardplays[index] = play;                                    // Update array with current play 
@@ -20,15 +21,16 @@
             if(winner) {
                 status.classList.add("you-won");
                 status.innerHTML = `Congratulations! ${winner} is the Winner!`;
-            } 
-            });
+                playOn = false;                                          // Prevent any additional plays after a game has been won
+            }
+             } });
         
         item.addEventListener("mouseover",()=> { 
-            item.classList.add("hover");                                 // Add appropriate styling to each square on mouseover   
+            item.classList.add("hover");                                  // Add appropriate styling to each square on mouseover   
             } );     
  
         item.addEventListener("mouseout",()=> { 
-            item.classList.remove("hover");                               // Add appropriate styling to each square on mouseout   
+            item.classList.remove("hover");                                // Add appropriate styling to each square on mouseout   
             } ); 
   
            
@@ -36,9 +38,9 @@
         });
 
     button.addEventListener("click",()=>{
-            boardplays      = new Array(9);                             // Clear array
-            board.forEach((item ) => {item.innerHTML = "";});           // Clear board
-            status.classList.remove("you-won");                         // remove you-won class from status div
+            boardplays      = new Array(9);                               // Create new array to store history
+            board.forEach((item ) => {item.innerHTML = "";});             // Clear board
+            status.classList.remove("you-won");                           // remove you-won class from status div
             status.innerHTML = "Move your mouse over a square and click to play an X or an O."; // Reset to default text
         });
 };
